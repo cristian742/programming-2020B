0101c201703c44a262e5c1658d23837962cd7816
¨cristian <geovannymunoz742@gmail.com>
2020-09-25 21:50:48 -0500

    calcular

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/CRISTIANO/Desktop/calculadora.py ==============
+resultado es : 11
+resultado es : -1
+resultado es: 30
+resultado es: 0.8333333333333334
+>>> 
+============== RESTART: C:/Users/CRISTIANO/Desktop/calculadora.py ==============
+resultado es : 11
+resultado es : -1
+resultado es: 30
+resultado es: 0.8333333333333334
+>>> 
+============== RESTART: C:/Users/CRISTIANO/Desktop/calculadora.py ==============
+resultado es : 11
+resultado es : -1
+resultado es: 30
+resultado es: 0.8333333333333334
+>>> 
