18855cb1e5887b5bd915830769f66b3bf61edce4
¨cristian <geovannymunoz742@gmail.com>
2020-09-26 00:34:36 -0500

    tabla2

---
+++ tabla 2.py
@@ -0,0 +1,27 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/CRISTIANO/Documents/tabla.py ================
+programa para que determine la tabla de un numero 
+incerta un numero 
+incerta un numero8
+Traceback (most recent call last):
+  File "C:/Users/CRISTIANO/Documents/tabla.py", line 7, in <module>
+    n=int(input ())
+ValueError: invalid literal for int() with base 10: 'incerta un numero8'
+>>> 
+================ RESTART: C:/Users/CRISTIANO/Documents/tabla.py ================
+programa para que determine la tabla de un numero 
+incerta un numero 
+3
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+>>> 
