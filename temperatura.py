5503860d2624dcfedbdb5a2b1877c570f71b7af8
¨cristian <geovannymunoz742@gmail.com>
2020-09-25 22:40:41 -0500

    temperatura

---
+++ temperatura.py
@@ -0,0 +1,15 @@
+#script: temperatura
+
+''' programa que calcula la temperatura
+'''
+
+n1=0
+n2=0
+
+print ('digite la temperatura en grados centrigrados')
+
+n1= int (input())
+
+n2= n1 + 273.15
+
+print ('la temperatura en grados K es ',n2)
